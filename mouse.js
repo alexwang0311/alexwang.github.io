198d22b732d4df2d63eef73b032898af94670a30
alexwang0311 <zhixiang.wang@vanderbilt.edu>
2022-12-20 10:15:01 -0600

    added event listeners for mobile devices

--- mouse.js
+++ mouse.js
@@ -1,3 +1,5 @@
+const smallDevice = window.matchMedia("(max-width: 576px)");
+
 const section = document.querySelector("#about");
 let zoomedIn = false;
 
@@ -5,8 +7,16 @@ const moveCircle = (e) => {
     const svg = document.querySelector(".o");
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-    const pctX = (e.clientX - vw / 2) / (vw / 2);
-    const pctY = (e.clientY - vh / 2) / (vh / 2);
+    let pctX = 0;
+    let pctY = 0;
+    if(smallDevice.matches) {
+        pctX = (e.touches[0].clientX - vw / 2) / (vw / 2);
+        pctY = (e.touches[0].clientY - vh / 2) / (vh / 2);
+    }
+    else {
+        pctX = (e.clientX - vw / 2) / (vw / 2);
+        pctY = (e.clientY - vh / 2) / (vh / 2);
+    }
     anime({
         targets: [".o-out"],
         translateX: pctX * svg.scrollWidth * 0.8 / 2,
@@ -41,15 +51,64 @@ const detachMouseMoveListener = () => {
     }
 }
 
-section.addEventListener("mouseenter", (e) => {
-    console.log("mouse entered about section");
-    attachMouseMoveListener();
-});
+const attachTouchMoveListener = () => {
+    if(!zoomedIn){
+        section.addEventListener("touchmove", moveCircle);
+        console.log("attached touch move listener");
+    }
+}
 
-section.addEventListener("mouseleave", (e) => {
-    console.log("mouse left about section");
-    detachMouseMoveListener();
-});
+const detachTouchMoveListener = () => {
+    if(!zoomedIn){
+        section.removeEventListener("touchmove", moveCircle);
+        console.log("removed touch move listener");
+        anime({
+            targets: [".o-out", ".o-in"],
+            translateX: 0,
+            translateY: 0,
+        });
+    }
+}
+
+const setUpListeners = () => {
+    if(smallDevice.matches) {
+        section.addEventListener("touchstart", (e) => {
+            console.log("touched about section");
+            attachTouchMoveListener();
+        });
+
+        section.addEventListener("touchend", (e) => {
+            console.log("ended touching about section");
+            detachTouchMoveListener();
+        });
+    }
+    else {
+        section.addEventListener("mouseenter", (e) => {
+            console.log("mouse entered about section");
+            attachMouseMoveListener();
+        });
+
+        section.addEventListener("mouseleave", (e) => {
+            console.log("mouse left about section");
+            detachMouseMoveListener();
+        });
+    }
+}
+
+setUpListeners();
+
+const lgText = `<h3>I'm Alex, a software engineer with a passion for crafting clean, modern, and efficient code.</h3>
+                <br>
+                <p>Whether I'm working on a new app or optimizing an existing system, I always strive to create a polished and intuitive user experience.</p>
+                <br>
+                <p>In my freetime, I work as a freelance full-stack web developer. While I'm not coding away on my new <a href="https://github.com/alexwang0311" target="_blank">projects</a>, I enjoy learning about 
+                    new technologies, reading, and working out. <a href="#">Here</a> is how I created this website.</p>`;
+const smText = `<h5>I'm Alex, a software engineer with a passion for crafting clean, modern, and efficient code.</h5>
+                <br>
+                <p>Whether I'm working on a new app or optimizing an existing system, I always strive to create a polished and intuitive user experience.</p>
+                <br>
+                <p>In my freetime, I work as a freelance full-stack web developer. While I'm not coding away on my new <a href="https://github.com/alexwang0311" target="_blank">projects</a>, I enjoy learning about 
+                    new technologies, reading, and working out. <a href="#">Here</a> is how I created this website.</p>`;
 
 const onClickHandler = (e) => {
     console.log("clicked");
@@ -81,38 +140,28 @@ const onClickHandler = (e) => {
                             .attr("class", "o-text")
                             .attr('transform', 'translate(' + [dx, dy] + ')');
                 const body = g.append("foreignObject")
-                    .attr("width", width)
-                    .attr("height", height)
-                    .style("overflow-y", "scroll")
-                    .append("xhtml:body")
-                    .style("font", "14px 'prompt-light'")
-                    .style("color", "white")
-                    .style("background-color", "transparent")
+                                .attr("width", width)
+                                .attr("height", height)
+                                .style("overflow-y", "scroll")
+                                .append("xhtml:body")
+                                .style("font", "14px 'prompt-light'")
+                                .style("color", "white")
+                                .style("background-color", "transparent");
                 if(isFullCircle){
-                    body.html(`<h3>I'm Alex, a software engineer with a passion for crafting clean, modern, and efficient code.</h3>
-                            <br>
-                            <p>Whether I'm working on a new app or optimizing an existing system, I always strive to create a polished and intuitive user experience.</p>
-                            <br>
-                            <p>In my freetime, I work as a freelance full-stack web developer. While I'm not coding away on my new <a href="https://github.com/alexwang0311" target="_blank">projects</a>, I enjoy learning about 
-                            new technologies, reading, and working out. <a href="#">Here</a> is how I created this website.</p>`);
+                    body.html(lgText);
                 }
                 else{
-                    body.html(`<h5>I'm Alex, a software engineer with a passion for crafting clean, modern, and efficient code.</h5>
-                            <br>
-                            <p>Whether I'm working on a new app or optimizing an existing system, I always strive to create a polished and intuitive user experience.</p>
-                            <br>
-                            <p>In my freetime, I work as a freelance full-stack web developer. While I'm not coding away on my new <a href="https://github.com/alexwang0311" target="_blank">projects</a>, I enjoy learning about 
-                            new technologies, reading, and working out. <a href="#">Here</a> is how I created this website.</p>`);
+                    body.html(smText);
                 }   
             }
         });
         anime({
             targets: ".AB",
-            translateX: "-20%"
+            translateX: "-40%"
         });
         anime({
             targets: ".UT",
-            translateX: "20%"
+            translateX: "40%"
         });
     }
     else{
@@ -148,6 +197,14 @@ const circle = document.querySelector(".o-group");
 circle.addEventListener("click", onClickHandler);
 
 addEventListener("resize", (e) => {
+    console.log("screen size changed");
+    if(smallDevice){
+        detachMouseMoveListener();
+    }
+    else{
+        detachTouchMoveListener();
+    }
+    setUpListeners();
     if(zoomedIn){
         const circle = document.querySelector(".o-in");
         const svg = document.querySelector(".o");
@@ -170,22 +227,12 @@ addEventListener("resize", (e) => {
         if(isFullCircle){
             xhtml.select("body")
                  .html("")
-                 .html(`<h3>I'm Alex, a software engineer with a passion for crafting clean, modern, and efficient code.</h3>
-                 <br>
-                 <p>Whether I'm working on a new app or optimizing an existing system, I always strive to create a polished and intuitive user experience.</p>
-                 <br>
-                 <p>In my freetime, I work as a freelance full-stack web developer. While I'm not coding away on my new <a href="https://github.com/alexwang0311" target="_blank">projects</a>, I enjoy learning about 
-                 new technologies, reading, and working out. <a href="#">Here</a> is how I created this website.</p>`);
+                 .html(lgText);
         }
         else{
             xhtml.select("body")
                  .html("")
-                 .html(`<h5>I'm Alex, a software engineer with a passion for crafting clean, modern, and efficient code.</h5>
-                 <br>
-                 <p>Whether I'm working on a new app or optimizing an existing system, I always strive to create a polished and intuitive user experience.</p>
-                 <br>
-                 <p>In my freetime, I work as a freelance full-stack web developer. While I'm not coding away on my new <a href="https://github.com/alexwang0311" target="_blank">projects</a>, I enjoy learning about 
-                 new technologies, reading, and working out. <a href="#">Here</a> is how I created this website.</p>`);
+                 .html(smText);
         }
     }
 });
