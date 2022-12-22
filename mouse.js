b7f2cba8a19b9266d8a350cb72ccf303f4e2ecff
alexwang0311 <zhixiang.wang@vanderbilt.edu>
2022-12-21 22:14:10 -0600

    fixed various issues loll

--- mouse.js
+++ mouse.js
@@ -1,6 +1,7 @@
 const smallDevice = window.matchMedia("(max-width: 576px)");
-
 const section = document.querySelector("#about");
+let isSmallDevice = smallDevice.matches;
+
 let zoomedIn = false;
 
 const lgMoveCircle = (e) => {
@@ -116,11 +117,13 @@ const setUpListeners = () => {
 
 setUpListeners();
 
-const lgText = `<h3>I am a software engineer with a passion for crafting clean, modern, and efficient solutions.</h3>
-                <br>
-                <p>Whether I'm working on a new app or optimizing an existing system, I always strive to create an intuitive and visually pleasing user experience - not settling for less.</p>
-                <p>In my freetime, I work as a freelance full-stack web developer. While I'm not coding away on my new <a href="https://github.com/alexwang0311" target="_blank">projects</a>, I enjoy learning about 
-                    digital design, reading about software architecture, and working out. <a href="#">Here</a> is how I created this website.</p>`;
+const lgText = `<div class="o-text-body">
+                    <h3>I am a software engineer with a passion for crafting clean, modern, and efficient solutions.</h3>
+                    <br>
+                    <p>Whether I'm working on a new app or optimizing an existing system, I always strive to create an intuitive and visually pleasing user experience - not settling for less.</p>
+                    <p>In my freetime, I work as a freelance full-stack web developer. While I'm not coding away on my new <a href="https://github.com/alexwang0311" target="_blank">projects</a>, I enjoy learning about 
+                        digital design, reading about software architecture, and working out. <a href="#">Here</a> is how I created this website.</p>
+                </div>`;
 const smText = `<div class="o-text-body" z-index="1">
                     <h4 class="o-text-body-header" style="font: 17px 'prompt-light'; opacity: 0">I am a software engineer with a passion for crafting clean, modern, and efficient solutions.</h4>
                     <br>
@@ -208,14 +211,18 @@ const lgOnClickHandler = (e) => {
         detachMouseMoveListener();
         zoomedIn = true;
         section.scrollIntoView();
+        const svg = document.querySelector(".o");
+        const w = svg.clientWidth;
+        const h = svg.clientHeight;
+        //radius percentage reference: https://oreillymedia.github.io/Using_SVG/extras/ch05-percentages.html
         anime({
             targets: ".o-out",
-            r: "32%",
+            r: (svg.scrollWidth / 2 - 1) / (Math.sqrt((Math.pow(w, 2) + Math.pow(h, 2)) / 2)) * 100 + "%",
             fill: "#000000",
         })
         anime({
             targets: [".o-in"],
-            r: "30%",
+            r: (svg.scrollWidth / 2 - 8) / (Math.sqrt((Math.pow(w, 2) + Math.pow(h, 2)) / 2)) * 100 + "%",
             fill: "#ffffff",
             duration: 500,
             delay: anime.stagger(100),
@@ -233,7 +240,7 @@ const lgOnClickHandler = (e) => {
                 const height = isFullCircle ? 2 * radius * Math.cos(Math.PI / 4) : 2 * Math.sqrt(Math.pow(radius, 2) - Math.pow(scrollWidth / 2, 2));
                 const dx = isFullCircle ? cx - radius * Math.sin(Math.PI / 4) : cx - scrollWidth / 2;
                 const dy = isFullCircle ? cy - radius * Math.cos(Math.PI / 4) : cy - height / 2;
-                //console.log(dx, dy);
+
                 const g = d3.select(".o")
                             .append('g')
                             .attr("class", "o-text")
@@ -244,17 +251,24 @@ const lgOnClickHandler = (e) => {
                                 .attr("height", height)
                                 .style("overflow-y", "scroll")
                                 .append("xhtml:body")
+                                .style("z-index", 1)
+                                .style("position", "relative")
                                 .style("font", "14px 'prompt-light'")
                                 .style("color", "black")
                                 .style("background-color", "transparent");
-                if(isFullCircle){
-                    body.html(lgText);
-                }
-                else{
-                    body.html(smText);
-                }
+                body.html(lgText);
                 document.querySelector(".o-text").addEventListener("click", lgOnClickHandler);
                 section.scrollIntoView();
+                //important!!! force the following code to be finish executing before anime({}) call
+                setTimeout(() => {
+                    const textBox = document.querySelector("foreignObject body div");
+                    const outerBox = document.querySelector(".o-text");
+                    const outerHeight = outerBox.getBoundingClientRect().height;
+                    console.log(textBox.clientHeight, outerHeight);
+                    if(textBox.clientHeight < outerHeight) {
+                        textBox.setAttribute("style", `transform: translateY(${(outerHeight - textBox.clientHeight) / 2}px)`);
+                    }
+                }, 0);
                 anime({
                     targets: ".o-text",
                     opacity: 1,
@@ -278,7 +292,6 @@ const lgOnClickHandler = (e) => {
         if(e.target.tagName.toLowerCase() == "a") return; //guard against click on 'a' elements
         zoomedIn = false;
         const el = document.getElementsByClassName("o-text");
-        //console.log(el);
         el[0].remove();
         attachMouseMoveListener();
         anime({
@@ -321,11 +334,11 @@ attachClickHandler();
 addEventListener("resize", (e) => {
     console.log("screen size changed");
     const circle = document.querySelector(".o-group");
-    if(smallDevice.matches){
-        circle.removeEventListener("click", lgOnClickHandler);
-        detachMouseMoveListener();
-        setUpListeners();
-        attachClickHandler();
+    if(smallDevice.matches && isSmallDevice){
+        //circle.removeEventListener("click", lgOnClickHandler);
+        //detachMouseMoveListener();
+        //setUpListeners();
+        //attachClickHandler();
         if(zoomedIn){
             const svg = document.querySelector(".o");
             const scrollHeight = svg.scrollHeight;
@@ -333,15 +346,20 @@ addEventListener("resize", (e) => {
                         .attr('transform', `translate(0, ${scrollHeight * 0.2})`);
         }
     }
-    else{
-        circle.removeEventListener("click", smOnClickHandler);
-        detachTouchMoveListener();
-        setUpListeners();
-        attachClickHandler();
+    else if(!smallDevice.matches && !isSmallDevice){
+        //circle.removeEventListener("click", smOnClickHandler);
+        //detachTouchMoveListener();
+        //setUpListeners();
+        //attachClickHandler();
         if(zoomedIn){
-            const circle = document.querySelector(".o-in");
             const svg = document.querySelector(".o");
             const scrollWidth = svg.scrollWidth;
+            const w = svg.clientWidth;
+            const h = svg.clientHeight;
+            document.querySelector(".o-out").setAttribute("r", (scrollWidth / 2 - 1) / (Math.sqrt((Math.pow(w, 2) + Math.pow(h, 2)) / 2)) * 100 + "%");
+            document.querySelector(".o-in").setAttribute("r", (scrollWidth / 2 - 8) / (Math.sqrt((Math.pow(w, 2) + Math.pow(h, 2)) / 2)) * 100+ "%");
+            const circle = document.querySelector(".o-in");
+            
             const radius = circle.r.animVal.value;
             const cx = circle.cx.animVal.value;
             const cy = circle.cy.animVal.value;
@@ -357,16 +375,33 @@ addEventListener("resize", (e) => {
             const xhtml = g.select("foreignObject")
                             .attr("width", width)
                             .attr("height", height);
-            if(isFullCircle){
-                xhtml.select("body")
-                     .html("")
-                     .html(lgText);
+
+            const textBox = document.querySelector("foreignObject body div");
+            const outerBox = document.querySelector(".o-text");
+            const outerHeight = outerBox.getBoundingClientRect().height;
+            if(textBox.clientHeight < outerHeight) {
+                //console.log(textBox.clientHeight, outerHeight);
+                textBox.setAttribute("style", `transform: translateY(${(outerHeight - textBox.clientHeight) / 2}px)`);
             }
             else{
-                xhtml.select("body")
-                     .html("")
-                     .html(smText);
+                textBox.setAttribute("style", `transform: translateY(0)`);
             }
         }
+    }
+    else if(smallDevice.matches && !isSmallDevice){
+        console.log("from lg device to sm device");
+        isSmallDevice = true;
+        circle.removeEventListener("click", lgOnClickHandler);
+        detachMouseMoveListener();
+        setUpListeners();
+        attachClickHandler();
+    }
+    else{
+        console.log("from sm device to lg device");
+        isSmallDevice = false;
+        circle.removeEventListener("click", smOnClickHandler);
+        detachTouchMoveListener();
+        setUpListeners();
+        attachClickHandler();
     }
 });
